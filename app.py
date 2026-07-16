48335e4316d5ec213cae054cee60e7cc9b43163f
mrparadox263-star <mrparadox263@gmail.com>
2026-07-16 11:36:46 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import stat
 app = Flask(__name__)
 
 # Find the exact folder where app.py lives
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(_file_))
 
 if platform.system() == "Windows":
     ENGINE_PATH = os.path.join(BASE_DIR, "patricia.exe")
